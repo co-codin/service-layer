693548eeeaa760d162882b2860d2c09ca31f78fb
elijah <cuiyeqing960904@gmail.com>
2023-03-07 16:28:50 -0500

    gracefully shudown

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -1,8 +1,13 @@
 package http
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -38,9 +43,25 @@ func (h *Handler) mapRoutes() {
 }
 
 func (h *Handler) Serve() error {
-	if err := h.Server.ListenAndServe(); err != nil {
-		return err
-	}
+	go func() {
+		if err := h.Server.ListenAndServe(); err != nil {
+			log.Println(err.Error())
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, os.Interrupt)
+
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+
+	defer cancel()
+
+	h.Server.Shutdown(ctx)
+
+	log.Println("shut down gracefully")
 
 	return nil
 }
