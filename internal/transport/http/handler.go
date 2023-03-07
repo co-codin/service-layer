dda3ba2a9fa0edab25bc3bee3218113ac7161e32
elijah <cuiyeqing960904@gmail.com>
2023-03-07 15:23:48 -0500

    server handler built

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -1,1 +1,46 @@
 package http
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type CommentService interface{}
+
+type Handler struct {
+	Router  *mux.Router
+	Service CommentService
+	Server  *http.Server
+}
+
+func NewHandler(servie CommentService) *Handler {
+	h := &Handler{
+		Service: servie,
+	}
+	h.Router = mux.NewRouter()
+
+	h.mapRoutes()
+
+	h.Server = &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: h.Router,
+	}
+
+	return h
+}
+
+func (h *Handler) mapRoutes() {
+	h.Router.handlerFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello world")
+	})
+}
+
+func (h *Handler) Serve() error {
+	if err := h.Server.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
