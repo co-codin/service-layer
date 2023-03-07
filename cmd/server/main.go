f9452b4ab048ca048055917a4956517b4dcb2770
elijah <cuiyeqing960904@gmail.com>
2023-03-07 05:54:45 -0500

    connected to db

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/co-codin/service-layer/internal/db"
+	"github.com/joho/godotenv"
+)
 
 func Run() error {
 	fmt.Println("starting up application")
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading environment variables file")
+	}
+
+	db, err := db.NewDatabase()
+
+	if err != nil {
+		fmt.Println("Failed to connect to database")
+		return err
+	}
+
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/co-codin/service-layer
 
 go 1.19
+
+require github.com/lib/pq v1.10.7
+
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
